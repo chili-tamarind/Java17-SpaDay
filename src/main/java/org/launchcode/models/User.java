a45e0c03f12ebb481f7f55860d760723c0cdcb97
chilli-tamarind <ceasinugo@randolphcollege.edu>
2024-10-19 22:50:48 -0500

    Chapter 15: Model Validation | Studio: Spa User Validation

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @Size(min= 5, max = 15, message = "Username should be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Email should be in a valid format")
     private String email;
+
+    @Size(min= 5, message = "Password should be at least 6 characters long")
     private String password;
 
-    public User() {
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
-    }
+    public User() {}
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
+
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -39,5 +52,22 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    public void checkPassword(){
+//        if (password != null || verifyPassword != null || !password.equals(verifyPassword))
+        if (password != null && verifyPassword != null && !password.equals(verifyPassword)) {
+            setVerifyPassword(null);
+        }
+    }
+
 }
 
